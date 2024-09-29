c6655fc1c180418f53bfe5cd37c13d742ed6e56c
somasekhar <ess527162@tatamotors.com>
2024-09-29 18:37:30 +0530

    Add route to say hello

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome</h1>"
 
+@app.route("/hello")
+def hello():
+    return "<h1>Hello World</h1>"
+
+
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port = 8080, debug = True)
