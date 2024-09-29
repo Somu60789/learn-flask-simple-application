7563f5e4be15c7da52e385792be2a8fe1bd8dd07
somasekhar <ess527162@tatamotors.com>
2024-09-29 18:48:24 +0530

    Add url processors

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ def index():
 def hello():
     return "<h1>Hello World</h1>"
 
+# URL processors 
+@app.route("/greet/<name>")
+def greet(name):
+    return f"Hello {name}"
+
+@app.route("/add/<int:number1>/<int:number2>")
+def addition(number1, number2):
+    return f"Sum of {number1} and {number2} is {number1 + number2}"
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port = 8080, debug = True)
