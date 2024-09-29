cb6a2e45738cdb738557d3ec15367ca1dbbf3493
somasekhar <ess527162@tatamotors.com>
2024-09-29 18:59:13 +0530

    Handeling url parameters

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -18,6 +18,16 @@ def greet(name):
 @app.route("/add/<int:number1>/<int:number2>")
 def addition(number1, number2):
     return f"Sum of {number1} and {number2} is {number1 + number2}"
+
+# Handelling url parameters
+@app.route("/handle_url_parameters")
+def handle_parms():
+    if "greetings" in request.args.keys() and "name" in request.args.keys():
+        greetings = request.args['greeting']
+        name = request.args['name']
+        return f"{greetings}, {name}"
+    else:
+        return "<h1>Some parameters are missing</h1>"
 
 if __name__ == '__main__':
     app.run( host='0.0.0.0', port = 8080, debug = True)
